88f1d64ef7deee22e955dcb2311544953e3098c6
Napoleon Stanley <napoleonstanley316@gmail.com>
2020-12-11 17:35:32 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -4,6 +4,27 @@
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+<!-- Your project description goes here. What problem did you solve? How did you solve it?
+
+This application has an input form that collects employee first name, last name, ID number, job title, annual salary.
+
+The input fields will be collected via jQuery in the .js script 
+
+The 'Submit' button:
+
+ Collects the form information
+ Stores the information to calculate monthly costs 
+ Appends information to the DOM 
+ Clears the input fields. 
+ 
+ The stored information, will be calculated and appended to the DOM. 
+ 
+ // CONDITIONAL INFORMATION
+ If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
+
+A delete button will remove an employee from the DOM. 
+
+ -->
+
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
---
+++ client.js
@@ -0,0 +1,77 @@
+console.log('js loaded!');
+
+let employeeInfo = [];
+
+
+$(document).ready(consoleLoaded);
+
+
+function consoleLoaded() {
+
+    console.log('jquery is ready');
+    $('#addEmployee').on('click', handleClick);
+    
+}
+
+
+
+function addInfo() {
+
+    console.log('addInfo Works!');
+
+
+    $('#employeeData').empty();
+
+    for (let employee of employeeInfo) {
+
+    $('#employeeData').append(`
+    <tr class="newInfo">
+     <td>${employee.last}</td>
+     <td>${employee.first}</td>
+     <td>${employee.ID}</td>
+     <td>${employee.title}</td>
+     <td>${employee.salary}</td>
+
+     </tr>`)
+    }
+   
+}
+
+function handleClick(){
+    console.log('clicked');
+ 
+    let newInfo = {
+       last: $('#last').val(),
+       first: $('#first').val(),
+       ID: $('#ID').val(),
+       title: $('#jobTitle').val(),
+       salary: $('#annualSalary').val(),
+
+    };
+    console.log(newInfo); // should be the newInfo object on the console
+    employeeInfo.push(newInfo);
+
+    console.log(employeeInfo);
+    
+    addInfo();
+    
+
+
+    emptyFields()
+
+
+}
+
+
+
+function emptyFields(){
+    $('#last').val('');
+    $('#first').val('');
+    $('#ID').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
+
+
+}
+
+
